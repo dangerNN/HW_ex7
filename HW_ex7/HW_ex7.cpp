2745b91dda158a706ac4e5cd856728f656a8f29e
DaniiLord <daniilsveckarev07@gmail.com>
2025-03-02 19:56:13 +0200

    Добавьте файлы проекта.

---
+++ HW_ex7/HW_ex7.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+    string S = "hello world. hello step. hello it.";
+    string S0 = "hello";
+    int count = 0; 
+
+    size_t pos = S.find(S0);
+
+    while (pos != string::npos) {
+        count++;
+        pos = S.find(S0, pos + S0.length());
+    }
+
+    cout <<S<< endl;
+    cout << "Количество слов hello: " << count << endl;
+
+    return 0;
+}
